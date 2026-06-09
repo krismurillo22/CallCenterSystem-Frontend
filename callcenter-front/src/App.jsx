d640868ec51881f18eaa3053abc51b0300698b3f
Sofia <scastelar16@gmail.com>
2026-06-08 19:33:50 -0600

    Componente reutilizable Title

--- callcenter-front/src/App.jsx
+++ callcenter-front/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import Title from './components/Title'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Title level={1}> Titulo principal </Title>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
